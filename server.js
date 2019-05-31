7ce05757eedab89cdd81850ab2298274498ddbd7
Gorilla0823 <44488380+Gorilla0823@users.noreply.github.com>
2019-05-31 14:15:12 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1leVRKnPfOIAD_hmM7jbS8stm6Y2qUrFaMxcHnjbdyAo';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -18,19 +18,33 @@ async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
   console.log(rows);
-
-  // TODO(you): Finish onGet.
-
-  res.json( { status: 'unimplemented'} );
+  var out = [] ,count=0;
+  if(rows.length<2)
+    res.json( { error : 'The List is empty.'} );
+  else{
+    for(let i=1;i<rows.length;i++){
+      let inner = {} ;
+      for(let j=0;j<rows[i].length;j++)    
+        inner[rows[0][j]]=rows[i][j];
+      out[count++]=inner;
+    }
+    res.json(out);
+  }
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
-
-  // TODO(you): Implement onPost.
-
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  var email,name;
+  console.log(email+'  '+name);
+  for(let title in messageBody){
+    if(title.toLowerCase()==="email") email=messageBody[title];
+    else if(title.toLowerCase()==="name") name=messageBody[title];
+  }
+  await sheet.appendRow([name,email]); 
+  res.json({ response:'success'});
 }
 app.post('/api', jsonParser, onPost);
 
@@ -38,27 +52,78 @@ async function onPatch(req, res) {
   const column  = req.params.column;
   const value  = req.params.value;
   const messageBody = req.body;
-
-  // TODO(you): Implement onPatch.
-
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  var fillin,column2;
+  for(let title in messageBody){
+      fillin=messageBody[title];
+      column2=title;
+  }
+  for(let i=1;i<rows.length;i++){
+      if(rows[0][0]===column.toLowerCase()){
+          if(rows[i][0].toLowerCase()===value.toLowerCase()
+              && column2.toLowerCase()===rows[0][1].toLowerCase()){
+              await sheet.setRow(i,[rows[i][0],fillin]);
+              break;
+          }
+          else if(rows[i][0].toLowerCase()===value.toLowerCase()
+              &&column2.toLowerCase()===rows[0][0].toLowerCase()){
+              await sheet.setRow(i,[fillin,rows[i][1]]);
+              break;
+          }
+      }
+      else{
+          if(rows[i][1]===value
+            &&column2.toLowerCase()===rows[0][0]){
+              await sheet.setRow(i,[fillin,rows[i][1]]);
+              break;
+          }
+          else if(rows[i][1]===value
+            &&column2.toLowerCase()===rows[0][1]){
+              await sheet.setRow(i,[rows[i][0],fillin]);
+              break;
+          }
+      }
+  }  
+  res.json({ response:'success'});
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
   const column  = req.params.column;
   const value  = req.params.value;
-
-  // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  var index=-1;
+  for(let i=0;i<rows[0].length;i++){
+      if(rows[0][i].toLowerCase()===column.toLowerCase()){
+          index=i;
+          break;
+      }
+  }
+  for(let i=1;i<rows.length;i++){
+      if(index!==-1){
+          if(index===0){
+              if(rows[i][index].toLowerCase()===value.toLowerCase()){
+                  await sheet.deleteRow(i);
+                  break;
+              }
+          }
+          else{
+              if(rows[i][index]===value){
+                  await sheet.deleteRow(i);
+                  break;
+              }
+          }
+      }
+  } 
+  res.json({ response:'success'});
 }
 app.delete('/api/:column/:value',  onDelete);
-
 
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+  console.log(`CS193X: Server listening on port ${port}!`);
 });
